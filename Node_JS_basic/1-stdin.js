1534128f4f5bd4fbdb64fe09a1c65a09f8728205
victormc13 <viktorsb14@gmail.com>
2024-02-27 18:40:33 -0500

    new: Create a program named that will be executed through command line taking an input and log it in stdout

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface({ input, output });
+
+
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing\n')
+})
+
