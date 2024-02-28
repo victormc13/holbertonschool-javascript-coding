3a1ce48781cbad728114a751bf00f3b82c2f6285
victormc13 <viktorsb14@gmail.com>
2024-02-27 22:01:01 -0500

    update: Updates code style fixing linter errors

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,14 +3,12 @@ const { stdin: input, stdout: output } = require('process');
 
 const rl = readline.createInterface({ input, output });
 
-
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
-
   rl.close();
 });
 
 rl.on('close', () => {
-  console.log('This important software is now closing\n')
-})
-
+  console.log('This important software is now closing\n');
+  process.exit(0);
+});
