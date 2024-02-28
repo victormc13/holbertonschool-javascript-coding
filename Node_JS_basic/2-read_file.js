0947a5685a590dff072685b6caa6aa9b409e9b9b
victormc13 <viktorsb14@gmail.com>
2024-02-28 01:00:21 -0500

    new: Adds 'countStudents' function that prints 'Number of students' and 'Number of students in [field]' to stdout

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents (filePath) {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.length > 0);
+    const students = lines.slice(1).map((line) => line.split(','));
+    const totalStudents = students.length;
+
+    const fieldCounts = students.reduce((acc, student) => {
+      const field = student[3];
+      acc[field] = (acc[field] ?? 0) + 1;
+      return acc;
+    }, {});
+
+    const formattedFieldLists = Object.entries(fieldCounts).map(([field, count]) => {
+      const studentNames = students
+        .filter((student) => student[3] === field)
+        .map((student) => student[0]);
+
+      return `Number of students in ${field}: ${count}. List: ${studentNames.join(', ')}`;
+    });
+
+    console.log(`Number of students: ${totalStudents}`);
+    formattedFieldLists.forEach((message) => console.log(message));
+  } else {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.expots = countStudents;
