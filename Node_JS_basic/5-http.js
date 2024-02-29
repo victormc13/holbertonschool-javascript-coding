374934116c1fcf99b6e3a4de2a73626c9c578c78
victormc13 <viktorsb14@gmail.com>
2024-02-28 20:26:54 -0500

    update: Fix case when endpoints '/students' work correctly, return the correct content, and the right status codes - without database

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -9,10 +9,14 @@ const app = http.createServer(async (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    res.end('This is the list of our students');
+
     const database = process.argv[2];
-    const message = await countStudents(database);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(message);
+    if (database) {
+      const message = await countStudents(database);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(message);
+    }
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not found');
