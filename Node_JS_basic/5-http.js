6d6500034e8a78e2fbb6617299c9a2ac49912da9
victormc13 <viktorsb14@gmail.com>
2024-02-28 22:34:44 -0500

    update: Updates error message of caught error in promise

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -17,10 +17,8 @@ const app = http.createServer((req, res) => {
         res.end('This is the list of our students\n' + data);
       }).catch((error) => {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('This is the list of our students\n' + error);
+        res.end('This is the list of our students\n' + error.message);
       });
-    } else {
-      res.end('This is the list of our students\nCannot load the database');
     }
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
