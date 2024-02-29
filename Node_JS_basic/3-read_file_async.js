ae032b8268620b4d5292aa099ce895193fb795d6
victormc13 <viktorsb14@gmail.com>
2024-02-28 21:10:53 -0500

    update: Rejects when error occurs and returns

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -4,11 +4,11 @@ function countStudents(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf-8', (err, data) => {
       if (err) {
-        return reject(new Error('Cannot load the database'));
+        reject(new Error('Cannot load the database'));
+        return
       }
 
       const lines = data
-        .toString()
         .split('\n')
         .filter((line) => line.length > 0);
       const students = lines.slice(1).map((line) => line.split(','));
